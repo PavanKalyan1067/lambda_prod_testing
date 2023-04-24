b26d78242dc00433b32f3faf05b9faf9bc09e78e
PavanKalyan1067 <100332055+PavanKalyan1067@users.noreply.github.com>
2023-04-24 15:45:54 +0530

    Add files via upload

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,33 +1,35 @@
-import boto3
 import json
-import os
+import boto3
 
-# set up clients
-s3 = boto3.client('s3')
-dynamodb = boto3.client('dynamodb')
+s3_client = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('abcd_table')
 
 def lambda_handler(event, context):
+    bucket_name = 'prod1data'
+    prefix = 'rawdata/'
     
-    # read the data from the source file
-    response = s3.get_object(Bucket='prod1data', Key='rawdata/sstudents_data.json')
-    data = response['Body'].read().decode('utf-8')
+    # List all JSON files in the S3 bucket with the given prefix
+    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+    files = [file['Key'] for file in response['Contents'] if file['Key'].endswith('.json')]
     
-    # transform the data
-    transformed_data = []
-    for record in json.loads(data):
-        transformed_record = {
-            'full_name': record['first_name'] + ' ' + record['last_name'],
-            'email': record['email']
-        }
-        transformed_data.append(transformed_record)
+    # Read each JSON file and insert data into DynamoDB table
+    for file in files:
+        obj = s3_client.get_object(Bucket=bucket_name, Key=file)
+        data = obj['Body'].read().decode('utf-8')
+            
+        transformed_data = []
+        for record in json.loads(data):
+            transformed_record = {
+                'full_name': record['first_name'] + ' ' + record['last_name'],
+                'email': record['email']
+            }
+            transformed_data.append(transformed_record)
     
     # write the transformed data to DynamoDB
-    table_name = 'abcd_table'
-    with dynamodb.batch_writer(TableName=table_name) as batch:
-        for record in transformed_data:
-            batch.put_item(Item=record)
-    
-    # return a success response
+        with table.batch_writer() as batch:
+            for record in transformed_data:
+                batch.put_item(Item=record)
     return {
         'statusCode': 200,
         'body': 'Data successfully transformed and written to DynamoDB.'
